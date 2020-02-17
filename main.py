41bd7cf0aa947fd93987d3ac478ad0de41f7ea6b
mortrick <david.yafe@gmail.com>
2020-02-18 00:12:30 +0200

    remove agg operation for testings

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ dist = ["USD"]
 for i in dist:
     cnf.load_cmc_data(i)
 
-mysql.qryexec(2) # Truncate the hourly percentage table
-mysql.qryexec(1) # Insert into hourly percentage table
+# mysql.qryexec(2) # Truncate the hourly percentage table
+# mysql.qryexec(1) # Insert into hourly percentage table
