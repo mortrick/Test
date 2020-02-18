f0fe69286793f3eeb4d8f953a65a3703afe5c238
mortrick <david.yafe@gmail.com>
2020-02-18 04:38:10 +0200

    Check linux version

---
+++ test_linux.py
@@ -0,0 +1,7 @@
+
+txt = "2020-02-18T01:07:08.000Z"
+
+print("Then len of txt is " + str(len(txt)))
+print("At the 10 point of the text you will find the sign " + str(txt[10]))
+print("At the 13 point of the text you will find the sign " + str(txt[13]))
+print("And the type of txt is " + str(type(txt)))
