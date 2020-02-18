c32ba17d3e6ffbaa475142ebc120c57e41298edc
mortrick <david.yafe@gmail.com>
2020-02-18 05:11:35 +0200

    Add check

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 import utils as cnf
 import mysqlactions as mysql
+import time
 
 dist = ["USD"]
 
 
 for i in dist:
     cnf.load_cmc_data(i, debugmode=1)
+time.sleep(5)
 
 mysql.qryexec(2) # Truncate the hourly percentage table
 mysql.qryexec(1) # Insert into hourly percentage table
